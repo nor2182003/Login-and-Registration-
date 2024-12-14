62e85f5715fda2d3a64379a3472438ddb5b97fae
nor2182003 <noranmohammed7579@gmail.com>
2024-12-15 02:22:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector(".wrapper");
+const loginlink = document.querySelector(".login-link");
+const registerlink = document.querySelector(".register-link");
+
+registerlink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
